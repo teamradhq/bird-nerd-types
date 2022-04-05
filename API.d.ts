879e469f1ac105c3edd1deda1795d009a7028be8
Paul Beynon <paul@beynon.app>
2022-04-05 16:07:41 +1000

    Extract types from bird-nerd-api

---
+++ API.d.ts
@@ -0,0 +1,34 @@
+declare module BirdNerd {
+  namespace Api {
+    type Payload<T> = Omit<T, '_id'>;
+
+    type CollectionData = {
+      status: number;
+      data: unknown;
+    };
+
+    type OkayPayload<T> = CollectionData & {
+      status: 200 | 201 | 202 | 203 | 204 | 205 | 206;
+      data: Payload<T>;
+    };
+
+    type ErrorPayload = CollectionData & {
+      status: 400 | 401 | 403 | 404 | 500 | 501 | 502 | 503 | 504;
+      data: {
+        error: string;
+      }
+    };
+
+    /**
+     * Properties required to define a set of API
+     * routes for interacting with a collection.
+     */
+    type RouteCollection<Schema, Model> = {
+      single: string;
+      plural: string;
+      Model: Model;
+    }
+  }
+
+}
+
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="./API.d.ts" />
 /// <reference path="./App.d.ts" />
 /// <reference path="./Data.d.ts" />
 /// <reference path="./JSON.d.ts" />
