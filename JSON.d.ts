3c4bd5c642e8ccba714fec6696f2d43368371d15
Paul Beynon <paul@beynon.app>
2022-04-07 10:25:55 +1000

    Fix invalid type references

--- JSON.d.ts
+++ JSON.d.ts
@@ -2,19 +2,19 @@ declare namespace BirdNerd {
   namespace JSON {
     /** JSON data extracted from source CSV data. */
     type BirdJSONData = {
-      families: Data.FamilyName[],
+      families: Names.Family[],
       familyData: Data.Family[],
       populationData: Data.Population[],
-      populations: Data.PopulationName[],
+      populations: Names.Population[],
       species: Data.Species[],
       statusData: Data.Status[],
-      statuses: Data.StatusName[],
+      statuses: Names.Status[],
     };
 
     /** JSON data scraped from wikipedia. */
     type WikiJSONData = {
-      families: Data.WikiFamily[];
-      species: Data.WikiSpecies[];
+      families: Wiki.Family[];
+      species: Wiki.Species[];
     }
   }
 }
